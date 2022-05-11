f0aa4b518ca96e131121762286c7dbb4d19609eb
Tarang2015 <45654414+Tarang2015@users.noreply.github.com>
2022-05-11 12:38:41 +0530

    Delete src directory

--- src/main/java/com/wellsfargo/yugabyteapp/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wellsfargo.yugabyteapp.repository;
-
-import com.wellsfargo.yugabyteapp.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
