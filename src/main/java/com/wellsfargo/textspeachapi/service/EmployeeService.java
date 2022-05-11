dca6825b89835e3de68228f18b572aed79d536fd
Tarang2015 <45654414+Tarang2015@users.noreply.github.com>
2022-05-11 17:33:52 +0530

    Update EmployeeService.java

--- src/main/java/com/wellsfargo/textspeachapi/service/EmployeeService.java
+++ src/main/java/com/wellsfargo/textspeachapi/service/EmployeeService.java
@@ -1,20 +1,35 @@
 package com.wellsfargo.textspeachapi.service;
 
 import com.wellsfargo.textspeachapi.model.Employee;
+import com.wellsfargo.textspeachapi.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
 
-    private final EmployeeService employeeService;
+    private final EmployeeRepository employeeRepository;
 
     @Autowired
-    public EmployeeService(EmployeeService employeeService) {
-        this.employeeService = employeeService;
-    }
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+     }
 
     public Employee findEmployee(String input){
-        return null;
+        //uid
+        if(input.startsWith("u")){
+            return employeeRepository.findByUid(input);
+        }//email
+        else if(input.startsWith("e")){
+            input = input.substring(1);
+            return employeeRepository.findByEmail(input);
+        }// emp id
+        else if(input.startsWith("ei")){
+            input = input.substring(2);
+            Integer empId = Integer.parseInt(input);
+            return employeeRepository.findByEmployeeId(empId);
+        }else{
+            return null;
+        }
     }
 }
