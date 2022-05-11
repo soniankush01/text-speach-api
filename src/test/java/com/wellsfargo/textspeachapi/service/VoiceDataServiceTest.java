9e7c6043c9956b695818b9f48ec055822982d4cc
Tarang2015 <45654414+Tarang2015@users.noreply.github.com>
2022-05-11 17:36:13 +0530

    Update VoiceDataServiceTest.java

--- src/test/java/com/wellsfargo/textspeachapi/service/VoiceDataServiceTest.java
+++ src/test/java/com/wellsfargo/textspeachapi/service/VoiceDataServiceTest.java
@@ -5,11 +5,21 @@ import com.wellsfargo.textspeachapi.model.VoiceData;
 import com.wellsfargo.textspeachapi.repository.VoiceDataRepository;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
+
 
 public class VoiceDataServiceTest {
 
@@ -21,11 +31,22 @@ public class VoiceDataServiceTest {
     @Before
     public void setup(){
         MockitoAnnotations.openMocks(this);
+
     }
 
     @Test
     public void testJsonToObject(){
         assertEquals(7,7);
+    }
+
+   // @Test
+    public void test_upload_voice() throws IOException {
+        VoiceData voiceData = new VoiceData();
+        MultipartFile file = new MockMultipartFile("voice.mp3","voice.mp3","text/audio",
+                new FileInputStream(new File(this.getClass().getResource("/voice.mp3").getFile())));
+        String empData = "{\"firstName\":\"Tribhuwan\",\"lastName\":\"Mahto\",\"email\":\"tribhuwan@wellsfargo.com\",\"legalName\":\"TribhuwanMahto\",\"preferredName\":\"Mahto\"}";
+        Mockito.doNothing().when(voiceDataRepository.save(voiceData));
+        voiceDataService.uploadVoice(file,empData);
     }
 
 }
